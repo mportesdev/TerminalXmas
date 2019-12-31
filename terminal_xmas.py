5c87099325e2edf3aca56f3ffae7fddbd712435a
myrmica-habilis <michalportes1@gmail.com>
2019-12-31 20:41:22 +0100

    Improve naming

--- terminal_xmas.py
+++ terminal_xmas.py
@@ -12,25 +12,25 @@ with open('house.txt') as f:
     house = list(ascii_house.rstrip())
 
 
-def colored_bulb(color):
+def get_bulb(color):
     color_code = {'yellow': 3, 'red': 1, 'green': 2, 'blue': 4,
                   'dark': 0}[color]
     return f'\033[9{color_code}m{BULB_CHAR}\033[0m'
 
 
-def lights(color, indexes):
-    off = True
+def switch_lights(color, indexes):
+    lights_off = True
 
     while True:
         for i in indexes:
-            house[i] = colored_bulb(color) if off else colored_bulb('dark')
+            house[i] = get_bulb(color) if lights_off else get_bulb('dark')
 
         mutex.acquire()
         os.system('cls' if os.name == 'nt' else 'clear')
         print(''.join(house))
         mutex.release()
 
-        off = not off
+        lights_off = not lights_off
 
         time.sleep(random.uniform(0.3, 0.6))
 
@@ -54,10 +54,10 @@ for i, char in enumerate(house):
         blue.append(i)
         house[i] = BULB_CHAR
 
-thread_yellow = threading.Thread(target=lights, args=('yellow', yellow))
-thread_red = threading.Thread(target=lights, args=('red', red))
-thread_green = threading.Thread(target=lights, args=('green', green))
-thread_blue = threading.Thread(target=lights, args=('blue', blue))
+thread_yellow = threading.Thread(target=switch_lights, args=('yellow', yellow))
+thread_red = threading.Thread(target=switch_lights, args=('red', red))
+thread_green = threading.Thread(target=switch_lights, args=('green', green))
+thread_blue = threading.Thread(target=switch_lights, args=('blue', blue))
 
 for thread in [thread_yellow, thread_red, thread_green, thread_blue]:
     thread.start()
