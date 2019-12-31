f9a5d79f41edabd7b23566a189d3d0cf9cb0fce1
myrmica-habilis <michalportes1@gmail.com>
2019-12-31 21:36:28 +0100

    Improve naming

--- terminal_xmas.py
+++ terminal_xmas.py
@@ -8,26 +8,27 @@ BULB_CHAR = '\N{BLACK STAR}'
 mutex = threading.Lock()
 
 with open('house.txt') as f:
-    ascii_house = f.read().partition('#')[0]
-    house = list(ascii_house.rstrip())
+    ascii_picture = f.read().partition('#')[0]
+    picture_data = list(ascii_picture.rstrip())
 
 
-def get_bulb(color):
+def get_bulb(color_char):
     color_code = {'Y': 3, 'R': 1, 'G': 2, 'B': 4,
-                  'dark': 0}[color]
+                  'dark': 0}[color_char]
     return f'\033[9{color_code}m{BULB_CHAR}\033[0m'
 
 
-def switch_lights(color, index_list):
+def switch_lights(color_char, index_list):
     lights_off = True
 
     while True:
         for index in index_list:
-            house[index] = get_bulb(color) if lights_off else get_bulb('dark')
+            picture_data[index] = (get_bulb(color_char)
+                                   if lights_off else get_bulb('dark'))
 
         mutex.acquire()
         os.system('cls' if os.name == 'nt' else 'clear')
-        print(''.join(house))
+        print(''.join(picture_data))
         mutex.release()
 
         lights_off = not lights_off
@@ -40,17 +41,18 @@ red = []
 green = []
 blue = []
 
-index_lists = {'Y': yellow, 'R': red, 'G': green, 'B': blue}
+index_list_for_char = {'Y': yellow, 'R': red, 'G': green, 'B': blue}
 
-for i, char in enumerate(house):
+for i, char in enumerate(picture_data):
     if char in ('Y', 'R', 'G', 'B'):
-        index_lists[char].append(i)
-        house[i] = BULB_CHAR
+        index_list_for_char[char].append(i)
+        picture_data[i] = BULB_CHAR
 
 threads = [threading.Thread(target=switch_lights, args=args)
-           for args in index_lists.items()]
+           for args in index_list_for_char.items()]
 
 for thread in threads:
     thread.start()
+
 for thread in threads:
     thread.join()
