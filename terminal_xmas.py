f23514dcd6108609209b95ae8066c2169d2f5c81
myrmica-habilis <michalportes1@gmail.com>
2020-01-01 14:04:17 +0100

    Move top-level code into main function

--- terminal_xmas.py
+++ terminal_xmas.py
@@ -5,6 +5,8 @@ import time
 
 BULB_CHAR = '\N{BLACK STAR}'
 
+mutex = threading.Lock()
+
 
 def get_bulb(color_char):
     color_code = {'Y': 3, 'R': 1, 'G': 2, 'B': 4,
@@ -30,26 +32,30 @@ def switch_lights(picture_data, color_char, index_list):
         time.sleep(random.uniform(0.3, 0.6))
 
 
-mutex = threading.Lock()
+def main():
 
-with open('house.txt') as f:
-    ascii_picture = f.read().partition('#')[0]
+    with open('house.txt') as f:
+        ascii_picture = f.read().partition('#')[0]
 
-picture_data = list(ascii_picture.rstrip())
+    picture_data = list(ascii_picture.rstrip())
 
-index_list_for_char = {}
+    index_list_for_char = {}
 
-for i, char in enumerate(picture_data):
-    if char in ('Y', 'R', 'G', 'B'):
-        index_list_for_char.setdefault(char, []).append(i)
-        picture_data[i] = BULB_CHAR
+    for i, char in enumerate(picture_data):
+        if char in ('Y', 'R', 'G', 'B'):
+            index_list_for_char.setdefault(char, []).append(i)
+            picture_data[i] = BULB_CHAR
 
-threads = [threading.Thread(target=switch_lights,
-                            args=(picture_data, color_char, index_list))
-           for color_char, index_list in index_list_for_char.items()]
+    threads = [threading.Thread(target=switch_lights,
+                                args=(picture_data, color_char, index_list))
+               for color_char, index_list in index_list_for_char.items()]
 
-for thread in threads:
-    thread.start()
+    for thread in threads:
+        thread.start()
 
-for thread in threads:
-    thread.join()
+    for thread in threads:
+        thread.join()
+
+
+if __name__ == '__main__':
+    main()
